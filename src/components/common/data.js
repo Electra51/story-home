1a523810fe9b360c4481369c7e2e38fad6559634
safayet@merchantbay.com <safayet@merchantbay.com>
2023-12-24 13:30:18 +0600

    responsive navbar added

---
+++ src/components/common/data.js
@@ -0,0 +1,8 @@
+export const menus = [
+  { to: "/home", label: "Home" },
+  { to: "/about", label: "About" },
+  { to: "/services", label: "Services" },
+  { to: "/project", label: "Project" },
+  { to: "/blog", label: "Blog" },
+  { to: "/contact", label: "Contact" },
+];
--- src/pages/ErrorPage/ErrorPage.jsx
+++ src/pages/ErrorPage/ErrorPage.jsx
@@ -11,13 +11,13 @@ const ErrorPage = () => {
         </div>
         <div className="md:ml-auto lg:ml-auto absolute top-60 md:top-52 lg:top-52 left-0 md:right-0 lg:right-0 w-[350px] md:w-[500px] lg:w-[500px]">
           {" "}
-          <h1 className="text-[190px] md:text-[250px] lg:text-[250px] font-black text-[#FA6410]">
+          <h1 className="text-[190px] md:text-[250px] lg:text-[250px] font-black text-[#FA6410] hover:text-paragraphColor">
             404
           </h1>
           <h1 className="text-center lg:text-start md:text-start text-2xl md:text-[35px] lg:text-[35px] font-normal w-[520px absolute bottom-24 right-0">
             We are sorry, but the page you requested was not found
           </h1>
-          <button className="border rounded-[18px] px-[40px] lg:px-[54px] py-[20px] lg:py-[26px] flex justify-center items-center gap-1 bg-[#FA6410] text-white hover:bg-paragraphColor hover:text-white mt-8 text-xl mx-auto lg:mx-0">
+          <button className="border rounded-[14px] px-[40px] lg:px-[40px] py-[20px] lg:py-[20px] flex justify-center items-center gap-1 bg-[#FA6410] text-white hover:bg-paragraphColor hover:text-white mt-8 text-xl mx-auto lg:mx-0">
             <MdArrowBack />
             <p className="text-[18px] font-semibold">Back to home</p>
           </button>
