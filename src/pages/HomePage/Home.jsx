d76affa6bab5fea6000ee509a98296622976ed60
safayet@merchantbay.com <safayet@merchantbay.com>
2023-12-25 13:59:06 +0600

    home 3 part with responsive

--- src/pages/HomePage/Home.jsx
+++ src/pages/HomePage/Home.jsx
@@ -1,12 +1,16 @@
 import React from "react";
 import Banner from "./Banner/Banner";
 import FeaturedServicePart from "./FeaturedServices/FeaturedServicePart";
+import EstimatePart from "./EstimatePart/EstimatePart";
+import REviewPart from "./ReviewPart/REviewPart";
 
 const Home = () => {
   return (
     <>
       <Banner />
       <FeaturedServicePart />
+      <EstimatePart />
+      {/* <REviewPart /> */}
     </>
   );
 };
---
+++ src/pages/HomePage/ReviewPart/REviewPart.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const REviewPart = () => {
+  return <div className="max-w-[1200px] mx-auto mt-[100px]">REviewPart</div>;
+};
+
+export default REviewPart;
